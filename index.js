80ba2f04c4285e1345828c16d42288977562200f
valeria caliguire <caliguireval@gmail.com>
2023-03-30 15:44:43 -0600

    iSureAmTheChampion

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove("main");
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = Valeria is the champion!;
